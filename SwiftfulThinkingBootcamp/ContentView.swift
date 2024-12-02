b47356efa3e0ea89d6c10db722051f487ed15232
Sebastian-Alexandru Popa <alexsebastian2006@yahoo.com>
2024-12-02 20:32:44 +0200

    Initial Commit

---
+++ SwiftfulThinkingBootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Sebastian-Alexandru Popa on 02.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcampApp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Sebastian-Alexandru Popa on 02.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
